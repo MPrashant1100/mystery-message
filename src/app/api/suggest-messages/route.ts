0b378219fec878fb9313db9e9209714dd6720c36
MPrashant1100 <prashantmishra1100@gmail.com>
2024-05-09 01:23:46 +0530

    backend done

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,40 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { NextRequest, NextResponse } from "next/server";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  try {
+    const prompt = ""
+    const response = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      max_tokens: 400,
+      stream: true,
+      prompt,
+    });
+
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      const { name, status, headers, message } = error;
+      return NextResponse.json(
+        {
+          name,
+          status,
+          headers,
+          message,
+        },
+        { status }
+      );
+    } else {
+      console.error("An unexpected error occured ", error);
+      throw error;
+    }
+  }
+}
